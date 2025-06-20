80fcb0e871d443c96a87d4231b07a6deca77963d
Sadavarte Shraddha <shraddhasadavarte24@gmail.com>
2025-06-20 12:22:22 +0530

    hollow rectangle

--- loops_in_java/src/loop/patterns.java
+++ loops_in_java/src/loop/patterns.java
@@ -13,5 +13,28 @@ public class patterns {
 			}
 			System.out.println("");
 		}
+	
+	//hollow rectangle
+//for right side => top to bottom j=1
+//    ***** => i=1 from top to bottom j=5=m
+//    *   *
+//    *   *
+//    *****	=>i=4=n consider matrix form (1,1) to (4,4)
+	//rows = outer loop, col = inner loop
+	System.out.println("=====================================");
+	int n=4;
+	int m=5;
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=m;j++) {
+			if(i==1 || i==n || j==1 || j==m) {
+				System.out.print("*");
+			}
+			else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}
+	
 	}
 }
