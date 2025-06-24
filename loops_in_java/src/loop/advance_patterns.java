f74b5723eff001aa7ae90c983bc6220cb25e63a4
Sadavarte Shraddha <shraddhasadavarte24@gmail.com>
2025-06-24 19:35:17 +0530

    diamond pattern

--- loops_in_java/src/loop/advance_patterns.java
+++ loops_in_java/src/loop/advance_patterns.java
@@ -123,6 +123,44 @@ public class advance_patterns {
 			}
 			System.out.println();
 		}
+		
+		System.out.println("======================");
+		//diamond patterns
+//		    *
+//		   ***
+//		  *****
+//		 *******
+//		 *******
+//		  *****
+//		   ***
+//		    *
+		//first half
+		for(int i=1;i<=n;i++) {
+			//spaces
+			for(int j=1;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			
+			//stars
+			for(int j=1;j<=2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		//second half
+		for(int i=n;i>=1;i--) {
+			//spaces
+			for(int j=1;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			
+			//stars
+			for(int j=1;j<=2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 
 }
